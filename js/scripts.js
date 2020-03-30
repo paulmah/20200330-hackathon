a975fdd50de44ac21be3fbaa8577b74f04c5b7d2
unknown <paulmah88@hotmail.com>
2020-03-30 16:01:35 -0600

    Ping Pong not working

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ console.log( "Redundant Function" );
 const f1 = redundant( "apple" );
 const f2 = redundant( "pear" );
 const f3 = redundant( "" );
-console.log( f1, f2, f3 );
 
 function redundant(str) {
 	return function redundantFunc() {  // help from bo to fix error   
@@ -12,3 +11,22 @@ function redundant(str) {
 }
 
 
+
+// edabit puzzle = Ping Pong! - not working
+// console.log( "Ping Pong!" );
+// console.log( pingPong(["Ping!", "Ping!", "Ping!"], true) );
+
+
+// function pingPong( arr, win ) {
+//     for ( i = 0; i <= arr.length * 2 ; i += 2 ) {
+//         if ( arr[i] === null ) {
+//             return arr;
+//         } else {
+//             if ( win = true ) {
+//                 arr.splice( i+1, 0, 'Pong!' );
+//             }
+//         }
+//     }
+// }
+
+``
